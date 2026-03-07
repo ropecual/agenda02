e3c9eb5087cf64a20d8e46ff511e01c3e3a8e5fb
Roni Ferraresso <rope.alvarenga@gmail.com>
2026-03-07 10:15:27 -0300

    Agenda 4 - Inclusão  do textechanged.  Busca no banco conforme digita.

--- Views/ListaProduto.xaml.cs
+++ Views/ListaProduto.xaml.cs
@@ -35,11 +35,28 @@ namespace agenda02.Views
         {
             Navigation.PushAsync(new NovoProduto());
         }
-
+        // Método anterior, aqui apertavamos o botão de busca e ele fazia a busca, agora vamos fazer a busca enquanto o usuário digita, usando o evento TextChanged
         private async void txt_search_SearchButtonPressed(object sender, EventArgs e)
         {
             var busca = txt_search.Text;
             var produtos = await App.Db.Search(busca);
+            lista.Clear();
+            foreach (var p in produtos)
+            {
+                lista.Add(p);
+            }
+        }
+
+        // Método novo, toda vez que o texto mudar, ele vai fazer a busca, sem precisar apertar o botão de busca
+        private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Pega o texto novo a cada letra digitada
+            string busca = e.NewTextValue;
+
+            // Chama a busca no banco (invoca a busca já feita no SQLiteDatabaseHelper)
+            var produtos = await App.Db.Search(busca);
+
+            // Atualiza a lista na tela
             lista.Clear();
             foreach (var p in produtos)
             {
